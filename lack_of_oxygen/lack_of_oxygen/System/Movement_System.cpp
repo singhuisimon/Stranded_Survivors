3bfeb33054c724e803ce8cc9d03c7dfbaad5bba0
ashleyThit <144297753+ashleyThit@users.noreply.github.com>
2024-09-27 11:26:12 +0800

    Added Movement cpp

--- lack_of_oxygen/lack_of_oxygen/System/Movement_System.cpp
+++ lack_of_oxygen/lack_of_oxygen/System/Movement_System.cpp
@@ -19,16 +19,63 @@ namespace lof {
         for (const auto& entity : entities) {
             EntityID id = entity->get_id();
 
-            // Check if entity has both Transform2D and Velocity_Component
+            Mass_Component* mass = nullptr;
+
+            //skip if the entity does not have mass
+            if (entity->has_component(ecs.get_component_id<Mass_Component>())) {
+
+                mass = &ecs.get_component<Mass_Component>(id);
+
+                std::cout << "Mass of obj: " << mass << std::endl;
+
+                //skip if the entity is static
+                if (mass->is_static) continue;
+            }
+
+
+            // Check if entity has both Transform2D, Velocity_Component
             if (entity->has_component(ecs.get_component_id<Transform2D>()) &&
-                entity->has_component(ecs.get_component_id<Velocity_Component>())) {
+                entity->has_component(ecs.get_component_id<Velocity_Component>()) &&
+                entity->has_component(ecs.get_component_id<Physics_Component>()))
+                {
 
                 Transform2D& transform = ecs.get_component<Transform2D>(id);
                 Velocity_Component& velocity = ecs.get_component<Velocity_Component>(id);
+                Physics_Component& physics = ecs.get_component<Physics_Component>(id);
+
+                if (mass && !mass->is_static) {
+
+                    //calculate the acceleration 
+
+                    Vec2D total_force = physics.accumulated_force + (physics.gravity * mass->mass); // F = ma = mg; g as acceleration: a(g) = F / m;
+                    Vec2D acceleration = total_force * mass->inv_mass; // F = ma; a = F / m;
+
+                    //update velocity according to the acceleration on each entity
+                    velocity.velocity += acceleration * delta_time;
+
+                }
+
+                //dampen velocity
+                velocity.velocity *= physics.damping_factor;
 
                 // Update position based on velocity and delta_time
-                transform.position.x += velocity.velocity.x * delta_time;
-                transform.position.y += velocity.velocity.y * delta_time;
+                transform.position += velocity.velocity * delta_time;
+
+                //clamp velocity to max velocity 
+                float squared_velocity = square_length_vec2d(velocity.velocity);
+
+                // length of entity's velocity > max_velocity
+                if (squared_velocity > physics.max_velocity * physics.max_velocity) {
+
+                    Vec2D normalize_result;
+                    normalize_vec2d(normalize_result, velocity.velocity);
+                    velocity.velocity = normalize_result * physics.max_velocity;
+                }
+
+
+                //reset the accumulated force 
+                physics.accumulated_force = Vec2D(0, 0);
+
             }
         }
     }
