657f5a61ed96d5c685b7e39e167469e18747ba5b
huishan.s <huishan0719@gmail.com>
2024-10-01 19:52:00 +0800

    update assets_manager.h
    just update code

--- lack_of_oxygen/lack_of_oxygen/Manager/Assets_Manager.h
+++ lack_of_oxygen/lack_of_oxygen/Manager/Assets_Manager.h
@@ -25,21 +25,23 @@ namespace lof
 	template <typename T>
 	class Assets_Manager: public manager
 	{
-		//std::shared_ptr<T> assets;
+		//std::shared_ptr<T> assets; map to  store assets with their names as keys
 		std::map<std::string, std::shared_ptr<T>> assets;
 
 	public:
 		//std::shared_ptr<T> Get_Texture(const std::string& name);
-		Load_Texture(std::string name, std::string file_Name);
+		void Load_Texture(std::string name, std::string file_Name);
 
-		Get_Texture(std::string name);
+		std::shared_ptr<T>Get_Texture(std::string name);
 
-		Load_Font(std::string name, std::string file_Name);
+		void Load_Font(std::string name, std::string file_Name);
 
-		Get_Font(std::string name);
+		std::shared_ptr<T>Get_Font(std::string name);
+
 	};
 
 } //end of namespace lof
+
 
 
 #endif // !LOF _ASSETS_MANAGER_H
