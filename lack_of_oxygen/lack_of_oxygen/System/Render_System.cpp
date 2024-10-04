4654cff0d67d9f3be6bf3cea46ab4636e53d2825
liliahana <liliana.hanawardani@gmail.com>
2024-10-04 11:19:51 +0800

    Added missing set_time to Render System.cpp

--- lack_of_oxygen/lack_of_oxygen/System/Render_System.cpp
+++ lack_of_oxygen/lack_of_oxygen/System/Render_System.cpp
@@ -14,7 +14,9 @@
 
 namespace lof {
 
-    Render_System::Render_System(ECS_Manager& ecs_manager) : ecs_manager(ecs_manager) {}
+    Render_System::Render_System(ECS_Manager& ecs_manager) : ecs_manager(ecs_manager) {
+        set_time(DEFAULT_START_TIME);
+    }
 
     std::string Render_System::get_type() const {
         return "Render_System";
