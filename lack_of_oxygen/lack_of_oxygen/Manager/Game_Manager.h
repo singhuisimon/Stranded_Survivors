24576e286ae381bf4920ad6bb0061a549b40c537
liliahana <liliana.hanawardani@gmail.com>
2025-01-31 16:50:06 +0800

    changed the current scene to 2

--- lack_of_oxygen/lack_of_oxygen/Manager/Game_Manager.h
+++ lack_of_oxygen/lack_of_oxygen/Manager/Game_Manager.h
@@ -56,7 +56,7 @@ namespace lof {
         int forces_flag = -1;
 
         // Flag for scn file
-        int current_scene = 1;
+        int current_scene = 2;
 
         // Top UI variables
         float current_oxygen_level = 100.0f;  // Start at 100%
