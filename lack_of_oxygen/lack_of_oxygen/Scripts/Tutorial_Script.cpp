ebda7377f8657c64192a5e85d42009471ec9ab55
Amanda Leow <amandaleow543@gmail.com>
2025-03-13 14:17:04 +0800

    commented out the mouse for now

--- lack_of_oxygen/lack_of_oxygen/Scripts/Tutorial_Script.cpp
+++ lack_of_oxygen/lack_of_oxygen/Scripts/Tutorial_Script.cpp
@@ -48,10 +48,11 @@ namespace lof {
                 return;
             }
             tutorial_script->check_keys();
-            tutorial_script->check_clicking_button(entity_id);
-            if (!tutorial_script->get_transitioning()) {
+            //tutorial_script->check_clicking_button(entity_id);
+           /* if (!tutorial_script->get_transitioning()) {
                 tutorial_script->check_pressing_button(entity_id);
-            }
+            }*/
+            tutorial_script->check_pressing_button(entity_id);
             tutorial_script->transit_next_scene();
 
             });
@@ -60,11 +61,11 @@ namespace lof {
 
     void Tutorial_Script::check_keys() {
         //check for keys here
-        left_mouse_last_frame = left_mouse_pressed;
+        //left_mouse_last_frame = left_mouse_pressed;
         key_a_last_frame = key_a_pressed;
         key_d_last_frame = key_d_pressed;
 
-        left_mouse_pressed = IM.is_mouse_button_held(GLFW_MOUSE_BUTTON_LEFT);
+        //left_mouse_pressed = IM.is_mouse_button_held(GLFW_MOUSE_BUTTON_LEFT);
         key_a_pressed = IM.is_key_held(GLFW_KEY_A);
         key_d_pressed = IM.is_key_held(GLFW_KEY_D);
 
@@ -132,17 +133,17 @@ namespace lof {
         //return false;
     }
 
-    bool Tutorial_Script::is_mouse_just_pressed(int button) {
-        bool is_pressed = (glfwGetMouseButton(window, button) == GLFW_PRESS) && !mouse_previous_state[button];
-        mouse_previous_state[button] = (glfwGetMouseButton(window, button) == GLFW_PRESS); // Update state
-        return is_pressed;
-    }
+    //bool Tutorial_Script::is_mouse_just_pressed(int button) {
+    //    bool is_pressed = (glfwGetMouseButton(window, button) == GLFW_PRESS) && !mouse_previous_state[button];
+    //    mouse_previous_state[button] = (glfwGetMouseButton(window, button) == GLFW_PRESS); // Update state
+    //    return is_pressed;
+    //}
 
-    bool Tutorial_Script::is_mouse_just_released(int button) {
-        bool is_released = (glfwGetMouseButton(window, button) == GLFW_RELEASE) && mouse_previous_state[button];
-        mouse_previous_state[button] = (glfwGetMouseButton(window, button) == GLFW_PRESS); // Update state
-        return is_released;
-    }
+    //bool Tutorial_Script::is_mouse_just_released(int button) {
+    //    bool is_released = (glfwGetMouseButton(window, button) == GLFW_RELEASE) && mouse_previous_state[button];
+    //    mouse_previous_state[button] = (glfwGetMouseButton(window, button) == GLFW_PRESS); // Update state
+    //    return is_released;
+    //}
 
     bool Tutorial_Script::get_transitioning() {
         return transitioning;
@@ -205,116 +206,116 @@ namespace lof {
         }
     }
 
-    void Tutorial_Script::check_clicking_button(EntityID entity_id) {
+    //void Tutorial_Script::check_clicking_button(EntityID entity_id) {
 
-        if (transitioning == true) {
-            return;
-        }
+    //    if (transitioning == true) {
+    //        return;
+    //    }
 
-        Vec2D world_mouse_pos = ESS.Get_World_MousePos();
+    //    Vec2D world_mouse_pos = ESS.Get_World_MousePos();
 
-        auto* entity = ECSM.get_entity(entity_id);
-        if (!entity) {
-            return;
-        }
+    //    auto* entity = ECSM.get_entity(entity_id);
+    //    if (!entity) {
+    //        return;
+    //    }
 
-        std::string entity_name = entity->get_name();
+    //    std::string entity_name = entity->get_name();
 
-        static const std::unordered_map<std::string, std::string> button_textures = {
-            {"a_button", "A_Batch_16"},
-            {"d_button", "D_Batch_16"},
-            {"e_long_button", "E_To_Begin_Batch_16"}
-        };
+    //    static const std::unordered_map<std::string, std::string> button_textures = {
+    //        {"a_button", "A_Batch_16"},
+    //        {"d_button", "D_Batch_16"},
+    //        {"e_long_button", "E_To_Begin_Batch_16"}
+    //    };
 
-        static const std::unordered_map<std::string, std::string> button_sounds = {
-            {"a_button", "tutorial_button"},
-            {"d_button", "tutorial_button"},
-            {"e_long_button", "main_menu"}
-        };
+    //    static const std::unordered_map<std::string, std::string> button_sounds = {
+    //        {"a_button", "tutorial_button"},
+    //        {"d_button", "tutorial_button"},
+    //        {"e_long_button", "main_menu"}
+    //    };
 
-        //return if the entity is not a button.
-        if (button_textures.find(entity_name) == button_textures.end()) {
-            return;
-        }
+    //    //return if the entity is not a button.
+    //    if (button_textures.find(entity_name) == button_textures.end()) {
+    //        return;
+    //    }
 
-        if (!ECSM.has_component<Transform2D>(entity_id) ||
-            !ECSM.has_component<Graphics_Component>(entity_id) ||
-            !ECSM.has_component<Audio_Component>(entity_id)) {
-            return;
-        }
+    //    if (!ECSM.has_component<Transform2D>(entity_id) ||
+    //        !ECSM.has_component<Graphics_Component>(entity_id) ||
+    //        !ECSM.has_component<Audio_Component>(entity_id)) {
+    //        return;
+    //    }
 
-        auto& transform = ECSM.get_component<Transform2D>(entity_id);
-        auto& graphics = ECSM.get_component<Graphics_Component>(entity_id);
-        auto& audio = ECSM.get_component<Audio_Component>(entity_id);
+    //    auto& transform = ECSM.get_component<Transform2D>(entity_id);
+    //    auto& graphics = ECSM.get_component<Graphics_Component>(entity_id);
+    //    auto& audio = ECSM.get_component<Audio_Component>(entity_id);
 
-        update_button_visibility(entity_id, graphics);
+    //    update_button_visibility(entity_id, graphics);
 
-        // Check if mouse is hovering over the button
-        bool is_hovered = ESS.Mouse_Over_AABB(
-            transform.position.x,
-            transform.position.y,
-            transform.scale.x,
-            transform.scale.y,
-            world_mouse_pos.x,
-            world_mouse_pos.y
-        );
+    //    // Check if mouse is hovering over the button
+    //    bool is_hovered = ESS.Mouse_Over_AABB(
+    //        transform.position.x,
+    //        transform.position.y,
+    //        transform.scale.x,
+    //        transform.scale.y,
+    //        world_mouse_pos.x,
+    //        world_mouse_pos.y
+    //    );
 
-        std::string base_texture = button_textures.at(entity_name);
-        std::string button_audio = button_sounds.at(entity_name);
+    //    std::string base_texture = button_textures.at(entity_name);
+    //    std::string button_audio = button_sounds.at(entity_name);
 
-        update_button_visibility(entity_id, graphics);
+    //    update_button_visibility(entity_id, graphics);
 
-        if (graphics.color.a < 1.0f) {
-            graphics.texture_name = base_texture + "_NORMAL";
+    //    if (graphics.color.a < 1.0f) {
+    //        graphics.texture_name = base_texture + "_NORMAL";
 
-            //set it to true to avoid playing the audio
-            tutorial_mouse_playing[entity_name] = true;
-            return;
-        }
+    //        //set it to true to avoid playing the audio
+    //        tutorial_mouse_playing[entity_name] = true;
+    //        return;
+    //    }
 
-       if (is_hovered) {
-            if (IM.is_mouse_button_held(GLFW_MOUSE_BUTTON_LEFT)) {
-                graphics.texture_name = base_texture + "_PRESSED";
-                if (tutorial_mouse_playing[entity_name] == false) {
-                    ADM.play_now(entity_id, button_audio, audio);
-                    tutorial_mouse_playing[entity_name] = true;
-                }
+    //   if (is_hovered) {
+    //        if (IM.is_mouse_button_released(GLFW_MOUSE_BUTTON_LEFT)) {
+    //            graphics.texture_name = base_texture + "_PRESSED";
+    //            if (tutorial_mouse_playing[entity_name] == false) {
+    //                ADM.play_now(entity_id, button_audio, audio);
+    //                tutorial_mouse_playing[entity_name] = true;
+    //            }
 
-                if (entity_name == "a_button") {
-                    std::cout << "a_button press" << entity_name << std::endl;
-                    set_tutorial_page(tutorial_page - 1);
-                    page_change = true;
-                    return;
-                }
-                else if (entity_name == "d_button") {
-                    std::cout << "d_button pressed" << entity_name << std::endl;
-                    set_tutorial_page(tutorial_page + 1);
-                    page_change = true;
-                    return;
-                }
-                else if (entity_name == "e_long_button") {
-                    if (tutorial_page == 10) {
-                        std::cout << entity_name << " from transiting" << std::endl;
-                        transitioning = true;
-                        return;
-                    }
-                }
-            }
-            else {
-                std::cout << "entity name from highlighted" << entity_name << std::endl;
-                tutorial_mouse_playing[entity_name] = false;
-                page_change = false;
-                graphics.texture_name = base_texture + "_HIGHLIGHTED";
-                return;
-            }
-        }
-        else {
-            //std::cout << "no changes is deteced" << std::endl;
-            page_change = false;
-            graphics.texture_name = base_texture + "_NORMAL";
-            tutorial_mouse_playing[entity_name] = false;
-        }
-    }
+    //            if (entity_name == "a_button") {
+    //                std::cout << "a_button press" << entity_name << std::endl;
+    //                set_tutorial_page(tutorial_page - 1);
+    //                page_change = true;
+    //                return;
+    //            }
+    //            else if (entity_name == "d_button") {
+    //                std::cout << "d_button pressed" << entity_name << std::endl;
+    //                set_tutorial_page(tutorial_page + 1);
+    //                page_change = true;
+    //                return;
+    //            }
+    //            else if (entity_name == "e_long_button") {
+    //                if (tutorial_page == 10) {
+    //                    std::cout << entity_name << " from transiting" << std::endl;
+    //                    transitioning = true;
+    //                    return;
+    //                }
+    //            }
+    //        }
+    //        else {
+    //            std::cout << "entity name from highlighted" << entity_name << std::endl;
+    //            tutorial_mouse_playing[entity_name] = false;
+    //            page_change = false;
+    //            graphics.texture_name = base_texture + "_HIGHLIGHTED";
+    //            return;
+    //        }
+    //    }
+    //    else {
+    //        //std::cout << "no changes is deteced" << std::endl;
+    //        page_change = false;
+    //        graphics.texture_name = base_texture + "_NORMAL";
+    //        tutorial_mouse_playing[entity_name] = false;
+    //    }
+    //}
 
     void Tutorial_Script::check_pressing_button(EntityID entity_id) {
         if (transitioning == true) {
@@ -363,6 +364,8 @@ namespace lof {
         auto& transform = ECSM.get_component<Transform2D>(entity_id);
         auto& graphics = ECSM.get_component<Graphics_Component>(entity_id);
         auto& audio = ECSM.get_component<Audio_Component>(entity_id);
+
+        update_button_visibility(entity_id, graphics);
 
         std::string base_texture = button_textures.at(entity_name);
         std::string button_audio = button_sounds.at(entity_name);
